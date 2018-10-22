d96bb8157d1e54d717cc5c9f6e862cff68e8e4ce
Sylvano <sylvanoverkuyl@hotmail.com>
2018-10-22 21:47:18 +0200

    Added custom error messages to registering

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -46,14 +46,20 @@ class RegisterController extends Controller
      * @param  array  $data
      * @return \Illuminate\Contracts\Validation\Validator
      */
+
+
     protected function validator(array $data)
     {
+        $message = array(
+            'student_number.digits' => 'The student number must contain at least :digits characters',
+            'email.regex'=> 'The email must end with rocfriesepoort.nl'
+        );
         return Validator::make($data, [
             'name' => 'required|string|max:255',
-            'email' => 'required|string|email|max:255|unique:users|regex:/rocfriesepoort/',
+            'email' => 'required|string|email|max:255|unique:users|regex:/rocfriesepoort.nl/',
             'password' => 'required|string|min:6|confirmed',
-            'student_number' => 'required|integer|min:6'
-        ]);
+            'student_number' => 'required|digits:6|integer'
+        ], $message);
     }
 
     /**
