c930b8cf0a29e382f372f1dd9621b83afe9ebfb0
jojomanoke <AQuariumvissen1>
2018-11-09 11:40:25 +0100

    Mail update

--- config/mail.php
+++ config/mail.php
@@ -56,8 +56,8 @@ return [
     */
 
     'from' => [
-        'address' => env('MAIL_FROM_ADDRESS', 'hello@example.com'),
-        'name' => env('MAIL_FROM_NAME', 'Example'),
+        'address' => env('MAIL_FROM_ADDRESS', 'passwordrecovery@martkrol.nl'),
+        'name' => env('MAIL_FROM_NAME', 'Dronessite'),
     ],
 
     /*
@@ -84,9 +84,9 @@ return [
     |
     */
 
-    'username' => env('MAIL_USERNAME'),
+    'username' => env('passwordrecovery@martkrol.nl'),
 
-    'password' => env('MAIL_PASSWORD'),
+    'password' => env('Password@123!'),
 
     /*
     |--------------------------------------------------------------------------
