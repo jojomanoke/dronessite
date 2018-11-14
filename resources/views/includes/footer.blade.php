3299ae866bcda38870caad5f2064cff495bc3329
Wagenpark RocIT <rick.smallenbroek@gmail.com>
2018-11-14 15:18:52 +0100

    ewg
    oke

--- resources/views/includes/footer.blade.php
+++ resources/views/includes/footer.blade.php
@@ -1,6 +1,40 @@
 </div>
-<footer>
-    @yield('footer')
+<div class="container">
+<footer >
+    <section id="contact">
+        <div class="section-content">
+            <h1 class="section-header">Get in <span class="content-header wow fadeIn " data-wow-delay="0.2s" data-wow-duration="2s"> Touch with us</span></h1>
+            <h3></h3>
+        </div>
+        <div class="contact-section">
+            <div class="container">
+
+                <form>
+                    <div class="row">
+                    <div class="col form-line">
+                        <div class="form-group">
+                            <label for="exampleInputEmail">Email Address</label>
+                            <input type="email" class="form-control" id="exampleInputEmail" placeholder=" Enter Email">
+                        </div>
+                    </div>
+
+                    <div class="col ">
+                        <div class="form-group">
+                            <label for ="description"> Message</label>
+                            <textarea  class="form-control" id="description" placeholder="Enter Your Message"></textarea>
+                        </div>
+                        <div>
+                            <button type="button" class="btn btn-default submit"><i class="fa fa-paper-plane" aria-hidden="true"></i>  Send Message</button>
+                        </div>
+
+                    </div>
+                    </div>
+                </form>
+
+            </div>
+        </div>
+    </section>
 </footer>
+</div>
 </body>
 </html>
