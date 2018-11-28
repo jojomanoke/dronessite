ef6b788bc452abbab9a04f80d3f9ce9314c69cae
Mart <martjkrol@outlook.com>
2018-11-28 14:41:25 +0100

    image login

--- resources/views/includes/head.blade.php
+++ resources/views/includes/head.blade.php
@@ -27,5 +27,5 @@
     <script type="text/javascript" src="//cdnjs.cloudflare.com/ajax/libs/cookieconsent2/1.0.10/cookieconsent.min.js"></script>
     <!-- End Cookie Consent plugin -->
 </head>
-<body {{ Request::path() == 'login' ? 'background-image' : '' }}>
+<body {{ Request::path() == 'login' ? 'class = background-image' : '' }}>
     <div id="app">
