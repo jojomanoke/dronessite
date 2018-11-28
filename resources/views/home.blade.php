a6de906600187d7f0fee06086daae1bb365e51b3
Mart <martjkrol@outlook.com>
2018-11-28 15:47:10 +0100

    added carousel to home page

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,22 +1,26 @@
 @extends('layouts.master')
 @section('content')
 <div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card border-dark-primary">
-                <div class="card-header bg-dark-primary">Dashboard</div>
-
-                <div class="card-body bg-dark-primary">
-                    @if (session('status'))
-                        <div class="alert alert-success" role="alert">
-                            {{ session('status') }}
-                        </div>
-                    @endif
-                        {{--@php dd(env('MAIL_HOST')) @endphp--}}
-                    You are logged in {{$user->name}}!
-                </div>
+    <div id="carouselExampleControls" class="carousel slide" data-ride="carousel">
+        <div class="carousel-inner">
+            <div class="carousel-item active">
+                <img class="d-block w-100" src="..." alt="First slide">
+            </div>
+            <div class="carousel-item">
+                <img class="d-block w-100" src="..." alt="Second slide">
+            </div>
+            <div class="carousel-item">
+                <img class="d-block w-100" src="..." alt="Third slide">
             </div>
         </div>
+        <a class="carousel-control-prev" href="#carouselExampleControls" role="button" data-slide="prev">
+            <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+            <span class="sr-only">Previous</span>
+        </a>
+        <a class="carousel-control-next" href="#carouselExampleControls" role="button" data-slide="next">
+            <span class="carousel-control-next-icon" aria-hidden="true"></span>
+            <span class="sr-only">Next</span>
+        </a>
     </div>
 </div>
 @endsection
