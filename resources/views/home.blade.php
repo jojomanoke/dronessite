d71695fbbdb7e00190051c899a5f69fc756f7148
Mart <martjkrol@outlook.com>
2018-12-05 11:08:14 +0100

    carousel edits

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -5,7 +5,7 @@
     <div id="carouselExampleControls" class="carousel slide" data-ride="carousel">
         <div class="carousel-inner">
             <div class="carousel-item active">
-                <img class="d-block w-100" src='{!!asset('storage/app/images/carousel-2.jpg')!!}' alt="First slide">
+                <img class="d-block w-100" src='{!!asset("storage/app/images/carousel-1.jpg")!!}' alt="First slide">
             </div>
             <div class="carousel-item">
                 <img class="d-block w-100" src='{!!asset("storage/app/images/carousel-2.jpg")!!}' alt="Second slide">
@@ -25,3 +25,4 @@
     </div>
 </div>
 @endsection
+
